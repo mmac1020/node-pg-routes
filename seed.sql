08fb3b3a60a8619817fd5f252f78708b6acdcb9d
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-11-16 16:00:08 -0500

    Changing seed file for 2111-fsa

--- seed.sql
+++ seed.sql
@@ -13,12 +13,12 @@ create table dogs (
 );
 
 INSERT INTO users (name, imageurl) values ('Mac', 'https://ca.slack-edge.com/T024FPYBQ-U017Z7YQ0SY-a18868e18f30-512');
-INSERT INTO users (name, imageurl) values ('Bart', 'https://ca.slack-edge.com/T024FPYBQ-U0210N4RHJT-ee19f7ce223d-512');
-INSERT INTO users (name, imageurl) values ('Anna', 'https://ca.slack-edge.com/T024FPYBQ-U01PC22BL3G-6a2da61fceac-512');
-INSERT INTO users (name, imageurl) values ('Dennese', 'https://ca.slack-edge.com/T024FPYBQ-U01HVGXSAUF-b9aa7c770294-512');
-INSERT INTO users (name, imageurl) values ('Em', 'https://ca.slack-edge.com/T024FPYBQ-U01PK1HCG9Z-0b369070f4cc-512');
-INSERT INTO users (name, imageurl) values ('Hannah', 'https://ca.slack-edge.com/T024FPYBQ-U01JGF6MJTE-c17540d15bbb-512');
-INSERT INTO users (name, imageurl) values ('Shirley', 'https://ca.slack-edge.com/T024FPYBQ-U0182PH4H7C-ab9bab30dc64-512');
+INSERT INTO users (name, imageurl) values ('Zach', 'https://ca.slack-edge.com/T2VHPBHA5-U02BKERCU78-c8d5447be016-512');
+INSERT INTO users (name, imageurl) values ('Sey', 'https://ca.slack-edge.com/T024FPYBQ-U01U52GE9HD-ad844432bdf3-512');
+INSERT INTO users (name, imageurl) values ('Amber', 'https://ca.slack-edge.com/T024FPYBQ-U0254SSL8TW-ed2db073e6f8-512');
+INSERT INTO users (name, imageurl) values ('Devonne', 'https://ca.slack-edge.com/T024FPYBQ-U024REQ113P-1c8254d97d77-512');
+INSERT INTO users (name, imageurl) values ('Eric', 'https://ca.slack-edge.com/T024FPYBQ-U029NPV4TNH-dfc7016d7417-512');
+INSERT INTO users (name, imageurl) values ('Greg', 'https://ca.slack-edge.com/T024FPYBQ-U029S12U8HH-ddf3217e4858-512');
 
 INSERT INTO dogs (name, imageurl) values ('Casey', 'https://i.ytimg.com/vi/mRf3-JkwqfU/sddefault.jpg');
 INSERT INTO dogs (name, imageurl) values ('Bubba', 'https://cf.ltkcdn.net/dogs/images/orig/236742-1600x1030-cutest-puppy-videos.jpg');
