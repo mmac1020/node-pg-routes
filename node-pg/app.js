747c89c07f5a6a4200267df2e7decc3ed33cc192
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2022-03-09 10:54:23 -0500

    Adding class notes

--- node-pg/app.js
+++ node-pg/app.js
@@ -1,3 +1,11 @@
+const pg = require('pg');
+
+// http://${location}:${port}
+// protocol://location/dbName
+const POSTGRES_URL = 'postgres://localhost/pg-node-rest'
+const client = new pg.Client(POSTGRES_URL);
+// const client2 = new pg.Client(otherURL)
+
 const prompt = (data) => {
   process.stdout.write(
     `Main Menu\nCommands:\n1. Select all users\n2. Select all puppies\n3. Exit\n> `
@@ -9,6 +17,14 @@ const main = async (data) => {
   const [command, arg] = entry.split(' ');
   switch (command) {
     case '1':
+      try {
+        const data = await client.query('SELECT name from users');
+        data.rows.forEach((row) => {
+          console.log(row);
+        })
+      } catch (err) {
+        console.log(err);
+      }
       break;
     case '2':
       break;
@@ -20,9 +36,15 @@ const main = async (data) => {
   prompt();
 };
 
-function start() {
-  prompt();
-  process.stdin.on('data', main);
+async function start() {
+  try {
+    await client.connect();
+    console.log('We are connected to postgres');
+    prompt();
+    process.stdin.on('data', main);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 start();
